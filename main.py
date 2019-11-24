fd7a3b029988ead825c47aa06e62c21c1b221a57
Lulzx <wiserishabh@gmail.com>
2019-11-24 13:13:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,71 +2,99 @@
 # -*- coding: utf-8 -*-
 import os
 import sys
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
 
-logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import json
+from lxml import html
+import apiai
+import requests
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
 logger = logging.getLogger(__name__)
 
 
-def start(bot, update):
-    update.message.reply_text('Hi! Send to me a software's name to find it's alternatives.')
+def start(update, context):
+    update.message.reply_text('Hi!')
 
 
-def help(bot, update):
-    update.message.reply_text('Help yourself on your own!')
+def textMessage(update, context):
+    headers = {
+        'Content-Type': 'application/json; charset=utf-8',
+        'Authorization': 'Bearer xyzpqrabclol',
+    }
+
+    data = '{"queryInput":{"text":{"text":"' + update.message.text + '","languageCode":"en"}},"queryParams":{"timeZone":"Asia/Kolkata"}}'
+
+    response = requests.post('https://dialogflow.googleapis.com/v2/projects/lulzx-4dd1d/agent/sessions/bfc25685-ba30-f876-a361-d0eb9167ce45:detectIntent', headers=headers, data=data)
+    response = response.json()
+    response = response['queryResult']['parameters']['q']
+    response = fetch(response)
+    update.message.reply_text(response)
 
 
-def show(bot, update):
-	post=""
-	pltoshow=""
-	altshow=""
-	tagshow=""
-	searched=update.message.text
-	urlsearched="http://alternativeto.net/browse/search/?q="+searched+"&ignoreExactMatch=true"
-	searchedpage = requests.get(urlsearched)
-	searchedtree = html.fromstring(searchedpage.content)
-	searchedlink=searchedtree.xpath('//a[@data-link-action="Search"]/@href')
-	url="http://alternativeto.net"+searchedlink[0]
-	page = requests.get(url)
-	tree = html.fromstring(page.content)
-	title=tree.xpath('//h1[@itemprop="name"]/text()')
-	tags=tree.xpath('//span[@class="label label-default"]/text()')
-	platforms=tree.xpath('//li[@class="label label-default "]/text()')
-	#image=tree.xpath('//div[@class="image-wrapper"]/img[@src]')
-	alternativs=tree.xpath('//a[@data-link-action="Alternatives"]/text()')
-	creatorwebsite=tree.xpath('//a[@class="ga_outgoing"]/@href')
-	post+="Titles:"+title[0]
-	for x in range(0,len(platforms)):
-		pltoshow+="#"+platforms[x]+"\n"
-	post+="Platforms :"+"\n"+pltoshow+"\n"
-	for y in range(0,len(alternativs)):
-		altshow+=alternativs[y]+"\n"
-	post+="Alternatives :"+"\n"+altshow+"\n"
-	for z in range(0,len(tags)):
-		tagshow+="#"+tags[z]+"\n"
-	post+="genres:"+"\n"+tagshow
-	try:
-		post+="Website:"+"\n"+creatorwebsite[0]
-	except IndexError:
-		print(title[0]+"Not Found !")
-	update.message.reply_text(post)
+def fetch(query):
+    post=""
+    pltoshow=""
+    altshow=""
+    tagshow=""
+    searched=query
+    urlsearched="http://alternativeto.net/browse/search/?q="+searched+"&ignoreExactMatch=true"
+    searchedpage = requests.get(urlsearched)
+    searchedtree = html.fromstring(searchedpage.content)
+    searchedlink=searchedtree.xpath('//a[@data-link-action="Search"]/@href')
+    url="http://alternativeto.net"+searchedlink[0]
+    page = requests.get(url)
+    tree = html.fromstring(page.content)
+    title=tree.xpath('//h1[@itemprop="name"]/text()')
+    tags=tree.xpath('//span[@class="label label-default"]/text()')
+    platforms=tree.xpath('//li[@class="label label-default "]/text()')
+    #image=tree.xpath('//div[@class="image-wrapper"]/img[@src]')
+    alternativs=tree.xpath('//a[@data-link-action="Alternatives"]/text()')
+    creatorwebsite=tree.xpath('//a[@class="ga_outgoing"]/@href')
+    try:
+        post+="{}[{}]\n".format(title[0], creatorwebsite[0])+"\n"
+    except IndexError:
+        post+=title[0]
+    for x in range(0,len(platforms)):
+        if x is len(platforms)-1:
+            pltoshow+="└ "+platforms[x]+"\n"
+        else:
+            pltoshow+="├ "+platforms[x]+"\n"
+    post+="Platforms: "+"\n"+pltoshow+"\n"
+    for y in range(0,len(alternativs)):
+        if y is len(alternativs)-1:
+            altshow+="└ "+alternativs[y]+"\n"
+        else:
+            altshow+="├ "+alternativs[y]+"\n"
+    post+="Alternatives: "+"\n"+altshow+"\n"
+    for z in range(0,len(tags)):
+	tagshow+="#"+tags[z]+"\n"
+    post+="genres:"+"\n"+tagshow
+    return post
 
 
-def error(bot, update):
-    logger.warning('Update "%s" caused error "%s"' % (update, error))
+def help(update, context):
+    update.message.reply_text('Help me instead, uhh!')
+
+
+def error(update, context):
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 
 def main():
     try:
-        token = sys.argv[1]
+        TOKEN = sys.argv[1]
     except IndexError:
-        token = os.environ.get("TOKEN")
-    updater = Updater(token)
+        TOKEN = os.environ.get("TOKEN")
+    updater = Updater(TOKEN, use_context=True)
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
-    dp.add_handler(MessageHandler(Filters.text, show))
+    dp.add_handler(MessageHandler(Filters.text, textMessage))
     dp.add_error_handler(error)
     updater.start_polling()
     logger.info("Ready to rock..!")
